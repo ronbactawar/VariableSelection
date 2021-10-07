42bf7beba031b32952a08dcb2681dfedf4e8f249
rbac <ronbactawar@gmail.com>
2021-10-07 16:49:06 -0400

    revise test

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,16 @@ using Test
 using DataFrames
 
 @testset "VariableSelector.jl" begin
-    df = DataFrame(
-    A = [1, 2, 3, 4, 5, 6], 
-    B = ["a", "b", "a", "b", "a", "b"],
-    C = ["c", "d", "c", "d", "c", "c"],
-    T = [.17, .23, .36, .46, .58, .67],
-    )
+
+    
+    con1 = [1, 2, 3, 4, 5, 6,7 ,8, 9, 10] 
+    con2 = [.17, .23, .36, .46, .58, .67, .77, .84, .92, 1.2]
+    cat1 = ["a", "b", "a", "b", "a", "b", "a", "c", "b", "a"]
+    cat2 = ["c", "d", "c", "d", "c", "c", "b", "c", "c", "c"]
+    
 
     #@test names(VariableSelector.variable_importance(df)) == ["feature", "test", "pvalue"]
-    @test 1 == 1
+    @test VariableSelector.gamma(con1, con2)[1] <= 1
+    @test VariableSelector.gamma(cat1, cat2)[1] <= 1
 
 end
