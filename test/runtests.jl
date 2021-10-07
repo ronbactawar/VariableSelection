088f0660c1f0ced9c74cb7cde93c3956d7982ad9
rbac <ronbactawar@gmail.com>
2021-10-07 16:26:02 -0400

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using VariableSelector
 using Test
+using DataFrames
 
 @testset "VariableSelector.jl" begin
-    # Write your tests here.
+    df = DataFrame(
+    A = [1, 2, 3, 4, 5, 6], 
+    B = ["a", "b", "a", "b", "a", "b"],
+    C = ["c", "d", "c", "d", "c", "c"],
+    T = [.17, .23, .36, .46, .58, .67],
+    )
+
+    #@test names(VariableSelector.variable_importance(df)) == ["feature", "test", "pvalue"]
+    @test 1 == 1
+
 end
